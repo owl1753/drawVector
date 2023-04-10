5d1275db7d63e7888ee6901d4a056e584dd806e2
owl1753 <bighammer1753@gmail.com>
2023-04-10 14:36:03 +0900

    add arrow and text

--- vector.js
+++ vector.js
@@ -1,7 +1,10 @@
 const G = 9.81
-const SCALE = 100
+const SCALE = 170
 const BASEX = 250
 const BASEY = 250
+const arrowLength = 15
+const arrowAngle = 30
+const textLength = 15
 
 const generateButton = document.getElementsByClassName("generate-button")[0]
 
@@ -19,10 +22,41 @@ function deg2rad(angle) {
 
 generateButton.addEventListener("click", () => {
     const svg = document.getElementsByTagName('svg')[0]
+
+    svg.replaceChildren()
+
+    const axis1 = makeSVGElement('line', { 
+        x1: 0,
+        y1: 250,
+        x2: 500,
+        y2: 250,
+        stroke: 'black',
+    });
+
+    const axis2 = makeSVGElement('line', { 
+        x1: 250,
+        y1: 0,
+        x2: 250,
+        y2: 500,
+        stroke: 'black',
+    });
+
+    svg.appendChild(axis1)
+    svg.appendChild(axis2)
+
     const forces = []
     const angles = []
     const color = ['red', 'green', 'blue', 'purple']
     const inputValues = document.getElementsByClassName("input-value")
+    let maxForce = 0
+    for (let i = 0; i < inputValues.length; i++) {
+        const force = parseFloat(inputValues[i].getElementsByTagName('div')[0].getElementsByTagName('input')[0].value) * G
+
+        if (maxForce < force) {
+            maxForce = force
+        }
+    }
+
     for (let i = 0; i < inputValues.length; i++) {
         const force = parseFloat(inputValues[i].getElementsByTagName('div')[0].getElementsByTagName('input')[0].value) * G
         const angle = parseFloat(inputValues[i].getElementsByTagName('div')[1].getElementsByTagName('input')[0].value)
@@ -30,22 +64,52 @@ generateButton.addEventListener("click", () => {
         forces.push(force)
         angles.push(angle)
 
+        const pointX = Math.cos(deg2rad(angle)) * (force / maxForce) * SCALE
+        const pointY = Math.sin(deg2rad(angle)) * (force / maxForce) * SCALE
+
         const line = makeSVGElement('line', { 
             x1: BASEX,
             y1: BASEY,
-            x2: BASEX + force * Math.cos(deg2rad(angle)) * SCALE,
-            y2: BASEY - force * Math.sin(deg2rad(angle)) * SCALE,
+            x2: BASEX + pointX,
+            y2: BASEY - pointY,
             stroke: color[i],
             "stroke-width": "2px",
         });
+        const arrow = makeSVGElement('polygon', {
+            points: `${BASEX + pointX},
+            ${BASEY - pointY} 
+            ${BASEX + pointX + Math.cos(deg2rad(angle + 180 - arrowAngle)) * arrowLength},
+            ${BASEY - pointY - Math.sin(deg2rad(angle + 180 - arrowAngle)) * arrowLength} 
+            ${BASEX + pointX + Math.cos(deg2rad(angle + 180 + arrowAngle)) * arrowLength},
+            ${BASEY - pointY - Math.sin(deg2rad(angle + 180 + arrowAngle)) * arrowLength}`,
+            fill: color[i],
+        })
+
+        const textX = pointX / 2
+        const textY = pointY / 2
+
+        const text = makeSVGElement('text', {
+            x: BASEX + textX + Math.cos(deg2rad(angle - 90)) * textLength,
+            y: BASEY - textY - Math.sin(deg2rad(angle - 90)) * textLength,
+            'text-anchor': 'middle',
+            'dominant-baseline': 'middle',
+        })
+
+        switch (i) {
+            case 0: text.textContent = 'A'; break;
+            case 1: text.textContent = 'B'; break;
+            case 2: text.textContent = 'C'; break;
+        }
 
         svg.appendChild(line);
+        svg.appendChild(arrow);
+        svg.appendChild(text);
     }
 
-    x1 = forces[0] * Math.cos(deg2rad(angles[0])) * SCALE
-    y1 = forces[0] * Math.sin(deg2rad(angles[0])) * SCALE
-    x2 = forces[1] * Math.cos(deg2rad(angles[1])) * SCALE
-    y2 = forces[1] * Math.sin(deg2rad(angles[1])) * SCALE
+    x1 = Math.cos(deg2rad(angles[0])) * (forces[0] / maxForce) * SCALE
+    y1 = Math.sin(deg2rad(angles[0])) * (forces[0] / maxForce) * SCALE
+    x2 = Math.cos(deg2rad(angles[1])) * (forces[1] / maxForce) * SCALE
+    y2 = Math.sin(deg2rad(angles[1])) * (forces[1] / maxForce) * SCALE
     x3 = x1 + x2
     y3 = y1 + y2
     const line1 = makeSVGElement('line', { 
@@ -75,7 +139,30 @@ generateButton.addEventListener("click", () => {
         "stroke-width": "2px",
         "stroke-dasharray": "2px",
     });
+    const arrow = makeSVGElement('polygon', {
+        points: `${BASEX + x3},
+        ${BASEY - y3} 
+        ${BASEX + x3 + Math.cos(Math.atan2(y3, x3) + deg2rad(180 - arrowAngle)) * arrowLength},
+        ${BASEY - y3 - Math.sin(Math.atan2(y3, x3) + deg2rad(180 - arrowAngle)) * arrowLength} 
+        ${BASEX + x3 + Math.cos(Math.atan2(y3, x3) + deg2rad(180 + arrowAngle)) * arrowLength},
+        ${BASEY - y3 - Math.sin(Math.atan2(y3, x3) + deg2rad(180 + arrowAngle)) * arrowLength}`,
+        fill: color[3],
+    })
+
+    const textX = x3 / 2
+    const textY = y3 / 2
+
+    const text = makeSVGElement('text', {
+            x: BASEX + textX + Math.cos(Math.atan2(y3, x3) - deg2rad(90)) * textLength,
+            y: BASEY - textY - Math.sin(Math.atan2(y3, x3) - deg2rad(90)) * textLength,
+            'text-anchor': 'middle',
+            'dominant-baseline': 'middle',
+        })
+
+    text.textContent = "R"
     svg.appendChild(line1);
     svg.appendChild(line2);
     svg.appendChild(line3);
+    svg.appendChild(arrow);
+    svg.appendChild(text);
 })
